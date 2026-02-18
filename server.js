a738b86ba806a6ec5139e5a5d4e2cbba50ac7363
adarsh161123 <adarsh.pandey@ens.enterprises>
2026-02-18 16:39:07 +0530

    add cors

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const Hotel = require("./models/hotel.js");
 const routes = require("./routes/personroute.js");
 const hotelRoutes = require("./routes/hotelroute.js");
 const passport = require("./auth.js");
+const cors = require("cors");
 require("dotenv").config();
 var express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = process.env.Port || 5000;
 app.use(bodyParser.json());
+app.use(cors());
+
 const middleware = (req, res, next) => {
   console.log("Middleware executed");
   console.log( `[${new Date().toISOString()}]  "Request URL:", ${req.url}`);
